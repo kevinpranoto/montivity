86d9af1349f6ba4661bb9f942ee0ac31dd616978
Kevin Pranoto <kevinpranoto97@gmail.com>
2018-02-03 12:11:40 -0800

    fortune cookie function

---
+++ montivity/Assets/Scripts/FortuneCookie.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FortuneCookie : MonoBehaviour {
+
+	public Text showQuote;
+
+	private List<string> quoteList;
+
+	// Use this for initialization
+	FortuneCookie()
+	{
+		quoteList = new List<string>();
+
+		quoteList.Add ("test fortune");
+		quoteList.Add ("test fortune1");
+		quoteList.Add ("test fortune2");
+		quoteList.Add ("test fortune3");
+		quoteList.Add ("test fortune4");
+		quoteList.Add ("test fortune5");
+		quoteList.Add ("test fortune6");
+
+	}
+
+	public void generateFortune()
+	{
+		showQuote.text = quoteList [Random.Range (0, (quoteList.Capacity) - 1)];
+	}
+}

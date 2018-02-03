0a89e42a4363fbbf9b83d0243075b0464c8bcd38
Kevin Pranoto <kevinpranoto97@gmail.com>
2018-02-03 09:19:57 -0800

    updating message and message logging

---
+++ montivity/Assets/Monster.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monster : MonoBehaviour {
+	public string name;
+	public string message;
+
+
+	public InputField newMessage;
+	public Text displayMesssage;
+
+
+	public Monster[] friends;
+	public List<string> feelsLog;
+	public Support[] incomingSupport;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	Monster()
+	{
+		name = "foo";
+		message = "";
+	}
+
+	public void setMessage()
+	{
+		displayMesssage.text = newMessage.text;
+		message = displayMesssage.text;
+		feelsLog.Add (message);
+		for (int i = 0; i < feelsLog.Count; ++i) 
+		{
+			Debug.Log (feelsLog [i]);
+		}
+	}
+
+}
---
+++ montivity/Assets/Support.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Support : MonoBehaviour {
+	public string message;
+	// emote object here;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	Support(string newMessage)
+	{
+		message = newMessage;
+	}
+
+	string getMessage()
+	{
+		return message;
+	}
+}
